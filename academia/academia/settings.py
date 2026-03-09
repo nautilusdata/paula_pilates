43daa9906a24eba10ce56f2d8e3b1c44e1f77766
nautilusdata <rayarza@nautilus.cl>
2026-03-09 02:19:41 -0300

    settings.py para Chile 'America/Santiago' las 4 lineas cambiaron (del setting horario)

--- academia/academia/settings.py
+++ academia/academia/settings.py
@@ -105,13 +105,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'  # Opcional: pone los mensajes en español de Chile
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = True  # Mantén esto en True para manejar cambios de horario de verano automáticamente
 
 
 # Static files (CSS, JavaScript, Images)
