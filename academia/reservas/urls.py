a9bd74a9fb65e7580dd47370ee71a0609ed8334c
nautilusdata <rayarza@nautilus.cl>
2026-03-16 02:44:11 -0300

    armando reservar pack reducido, partimos con resrevas.html y las urls de reservas

--- academia/reservas/urls.py
+++ academia/reservas/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('reservar/pack10/confirmar/', views_reservar_pack10.reservar_pack10_confirmar, name='reservar_pack10_confirmar'),
     path('reservar/pack10/horas/', views_reservar_pack10.horas_disponibles_ajax, name='horas_disponibles_ajax'),
     path('mis-clases/', views_mis_clases.mis_clases, name='mis_clases'),
+    path('reservar/pack-reducido/', views_reservar_pack10.reservar_pack_reducido, name='reservar_pack_reducido'),
+    path('reservar_pack_reducido/confirmar/', views_reservar_pack10.reservar_pack_reducido_confirmar, name='reservar_pack_reducido_confirmar'),
 ]
