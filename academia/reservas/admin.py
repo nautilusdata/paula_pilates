e7be937591523e0fbc9bbba765808f0fb5699cf8
nautilusdata <rayarza@nautilus.cl>
2026-03-22 22:52:26 -0300

    agregando al admin para ver precios

--- academia/reservas/admin.py
+++ academia/reservas/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import UserMetadata, Pack, Sesion
-from .models import UserMetadata, Pack, Sesion, ConfiguracionHorario
+from .models import UserMetadata, Pack, Sesion, ConfiguracionHorario, ConfiguracionGeneral, ConfiguracionPrecio
+
 
 admin.site.register(UserMetadata)
 admin.site.register(Pack)
 admin.site.register(Sesion)
 admin.site.register(ConfiguracionHorario)
+admin.site.register(ConfiguracionGeneral)
+admin.site.register(ConfiguracionPrecio)
