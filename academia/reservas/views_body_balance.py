07a26e50c698c1e97a53a3833f8bf4817752b356
nautilusdata <rayarza@nautilus.cl>
2026-04-08 02:09:02 -0400

    se quitó hardcodeo url de retorno ngrok por una dinámica lista para produccion igual a caso anterior

--- academia/reservas/views_body_balance.py
+++ academia/reservas/views_body_balance.py
@@ -167,7 +167,7 @@ def reservar_body_balance_confirmar(request):
             del request.session['bb_borrador']
 
             from .views_webpay import crear_transaccion
-            return_url = 'https://gabriela-nonacceleratory-nonelectrically.ngrok-free.dev/pago/webpay/retorno/'
+            return_url = request.build_absolute_uri('/pago/webpay/retorno/')
             data = crear_transaccion(pack, return_url)
 
             token = data.get('token')
