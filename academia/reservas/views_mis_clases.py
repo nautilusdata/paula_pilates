f6bdd01490bbe68eb43a7dff93fe77482ff5dcc9
nautilusdata <rayarza@nautilus.cl>
2026-04-08 02:29:29 -0400

    excluimos de verpacks huérfanos

--- academia/reservas/views_mis_clases.py
+++ academia/reservas/views_mis_clases.py
@@ -21,7 +21,7 @@ def mis_clases(request):
     packs = (
         Pack.objects
         .filter(alumna=request.user)
-        .exclude(estado='CANCELADO')
+        .exclude(estado__in=['CANCELADO', 'PENDIENTE_PAGO']) # excluyendo mostrar packs huerfanos
         .prefetch_related('sesiones')
         .order_by('-fecha_inicio')
     )
