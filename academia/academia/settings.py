3876592ab82b08db2457a30cf37b3ba147ebcaaa
nautilusdata <rayarza@nautilus.cl>
2026-04-19 13:45:33 -0400

    allowed hosts corregido, en gcp estaba dando Bad Request

--- academia/academia/settings.py
+++ academia/academia/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
 # Leemos el texto del .env y lo dividimos por las comas
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', '192.168.43.170', 'gabriela-nonacceleratory-nonelectrically.ngrok-free.dev']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 CSRF_TRUSTED_ORIGINS = ['https://gabriela-nonacceleratory-nonelectrically.ngrok-free.dev']
 
 # Application definition
