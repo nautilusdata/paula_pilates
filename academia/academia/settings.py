b02b5de7bbacfb2c672d1a4d935bdbfc97f0a1c1
nautilusdata <rayarza@nautilus.cl>
2026-05-15 00:09:27 -0400

    delta recomendado por Gemini por error 500 de login

--- academia/academia/settings.py
+++ academia/academia/settings.py
@@ -119,7 +119,8 @@ STATICFILES_DIRS = [
 # Opcional pero útil: donde se copian con collectstatic
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
