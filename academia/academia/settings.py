b27ed2ad407e7902811d66fc209d6e3664dc87ae
nautilusdata <rayarza@nautilus.cl>
2026-05-15 00:14:28 -0400

    no resolvió nada lo anerior, vuelvo al original

--- academia/academia/settings.py
+++ academia/academia/settings.py
@@ -119,8 +119,7 @@ STATICFILES_DIRS = [
 # Opcional pero útil: donde se copian con collectstatic
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
