8c52372d4cd1bcd2960e28b895c2a96dd6d2b57d
nautilusdata <rayarza@nautilus.cl>
2026-07-22 23:33:54 -0400

    grasa off de settings.py (#comentarios)

--- academia/academia/settings.py
+++ academia/academia/settings.py
@@ -9,12 +9,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# Obtener la clave secreta desde el archivo .env
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -72,9 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'academia.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 # Configuración de PostgreSQL usando la URL del archivo .env
 DATABASES = {
